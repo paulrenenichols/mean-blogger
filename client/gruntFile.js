b599c44fe92c090adfade3e9a6e7cb979d8097dc
Paul Nichols <paul.rene.nichols@gmail.com>
2014-03-09 10:43:37 -0500

    updated the client app's grunt file

--- client/gruntFile.js
+++ client/gruntFile.js
@@ -7,7 +7,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-karma');
 
   // Default task.
