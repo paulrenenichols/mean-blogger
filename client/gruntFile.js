730e297bc9304c89e163faa34b66fe4d457cb2c1
Paul Nichols <paul.rene.nichols@gmail.com>
2014-03-08 21:42:23 -0600

    Client is stubbed out and displays 'Hello World'

---
+++ client/gruntFile.js
@@ -0,0 +1,149 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-recess');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint','build','karma:unit']);
+  grunt.registerTask('build', ['clean','concat','sass:build','copy:assets']);
+  grunt.registerTask('release', ['clean','uglify','jshint','karma:unit','concat:index', 'sass:min','copy:assets']);
+  grunt.registerTask('test-watch', ['karma:watch']);
+
+  // Print a timestamp (useful for when watching)
+  grunt.registerTask('timestamp', function() {
+    grunt.log.subhead(Date());
+  });
+
+  var karmaConfig = function(configFile, customOptions) {
+    var options = { configFile: configFile, keepalive: true };
+    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
+    return grunt.util._.extend(options, customOptions, travisOptions);
+  };
+
+  // Project configuration.
+  grunt.initConfig({
+    distdir: 'dist',
+    pkg: grunt.file.readJSON('package.json'),
+    banner:
+    '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
+    ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
+    src: {
+        js: ['src/**/*.js'],
+        html: ['src/index.html'],
+        sass: ['src/sass/main.scss'],
+        sassWatch: ['src/sass/**.css']
+
+    },
+    dest: {
+        css: '<%= distdir %>/<%= pkg.name %>.css'
+    },
+    clean: ['<%= distdir %>/*'],
+    copy: {
+      assets: {
+        files: [{ dest: '<%= distdir %>', src : '**', expand: true, cwd: 'src/assets/' }]
+      }
+    },
+    karma: {
+      unit: { options: karmaConfig('test/config/unit.js') },
+      watch: { options: karmaConfig('test/config/unit.js', { singleRun:false, autoWatch: true}) }
+    },
+    concat:{
+      dist:{
+        options: {
+          banner: "<%= banner %>"
+        },
+        src:['<%= src.js %>'],
+        dest:'<%= distdir %>/<%= pkg.name %>.js'
+      },
+      index: {
+        src: ['src/index.html'],
+        dest: '<%= distdir %>/index.html',
+        options: {
+          process: true
+        }
+      },
+      angular: {
+        src:['vendor/angular/angular.js', 'vendor/angular/angular-*.js'],
+        dest: '<%= distdir %>/angular.js'
+      },
+      bootstrap: {
+        src:['vendor/angular-ui/bootstrap/*.js'],
+        dest: '<%= distdir %>/bootstrap.js'
+      }
+    },
+    uglify: {
+      dist:{
+        options: {
+          banner: "<%= banner %>"
+        },
+        src:['<%= src.js %>' ,'<%= src.jsTpl %>'],
+        dest:'<%= distdir %>/<%= pkg.name %>.js'
+      },
+      angular: {
+        src:['<%= concat.angular.src %>'],
+        dest: '<%= distdir %>/angular.js'
+      },
+      bootstrap: {
+        src:['vendor/angular-ui/bootstrap/*.js'],
+        dest: '<%= distdir %>/bootstrap.js'
+      },
+      jquery: {
+        src:['vendor/jquery/*.js'],
+        dest: '<%= distdir %>/jquery.js'
+      }
+    },
+    sass: {
+        build: {
+            options: {
+                style: 'expanded'
+            },
+            files: {
+                '<%= dest.css %>': '<%= src.sass %>'
+            }
+        },
+        min: {
+            options: {
+                style: 'compressed'
+            },
+            files: {
+                '<%= dest.css %>': '<% =src.sass %>'
+            }
+        }
+    },
+    watch:{
+      all: {
+        files:['<%= src.js %>', '<%= src.html %>', '<%= src.sassWatch %>'],
+        tasks:['default','timestamp']
+      },
+      build: {
+        files:['<%= src.js %>', '<%= src.html %>', '<%= src.sassWatch %>'],
+        tasks:['build','timestamp']
+      }
+    },
+    jshint:{
+      files:['gruntFile.js', '<%= src.js %>'],
+      options:{
+        curly:true,
+        eqeqeq:true,
+        immed:true,
+        latedef:true,
+        newcap:true,
+        noarg:true,
+        sub:true,
+        boss:true,
+        eqnull:true,
+        globals:{}
+      }
+    }
+  });
+
+};
---
+++ client/src/app/app.js
@@ -0,0 +1,12 @@
+angular.module('meanBlogger', [
+  'ngRoute'
+])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $routeProvider.otherwise({redirectTo:'/'});
+    }])
+    .controller('MeanBloggerController', function($scope) {
+        $scope.msg = "Hello World";
+    });
+
+
