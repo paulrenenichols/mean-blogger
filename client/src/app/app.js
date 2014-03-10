9bd0b1fc32f17d60d56564e968576058b29f0195
Paul Nichols <paul.rene.nichols@gmail.com>
2014-03-10 11:14:44 -0500

    Now it says Hello World, Dan

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -6,7 +6,7 @@ angular.module('meanBlogger', [
       $routeProvider.otherwise({redirectTo:'/'});
     }])
     .controller('MeanBloggerController', function($scope) {
-        $scope.msg = "Hello World, Paul";
+        $scope.msg = "Hello World, Dan";
     });
 
 
