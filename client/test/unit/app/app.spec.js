a3bb30699978151c5a8ada809a9aec53e7a5d862
Paul Nichols <paul.rene.nichols@gmail.com>
2014-03-11 17:28:10 -0500

    Put Angular test back in.  Reduced Karma logging to LOG_INFO.

--- client/test/unit/app/app.spec.js
+++ client/test/unit/app/app.spec.js
@@ -1,16 +1,17 @@
 describe('mean-blogger', function() {
 
-//    beforeEach(module('mean-blogger'));
-//
-//    describe('MeanBloggerController', function() {
-//        it('should add a msg property to $scope', inject(function($controller, $rootScope) {
-//            var locals = {
-//                $scope: $rootScope.$new()
-//            };
-//            var ctrl = $controller('MeanBloggerController', locals);
-//            expect(locals.$scope.msg).toBeDefined();
-//        }));
-//    });
+    beforeEach(module('mean-blogger'));
+
+    describe('MeanBloggerController', function() {
+        it('should add a msg property to $scope', inject(function($controller, $rootScope) {
+            var locals = {
+                $scope: $rootScope.$new()
+            };
+            var ctrl = $controller('MeanBloggerController', locals);
+            expect(locals.$scope.msg).toBeDefined();
+        }));
+    });
+
     describe('true is true', function() {
         it('true should be true', function() {
             expect(true).toBe(true);
